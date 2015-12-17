b9647c36c3202d44e87b5ccb02d19d5edd8f6154
Tristan Jay Mahr <Mahr@Wisc.Edu>
2015-12-17 09:53:57 -0600

    rename stan example files

--- misc/bulk_rename.R
+++ misc/bulk_rename.R
@@ -7,12 +7,14 @@ numbered_examples <- all_files %>%
   str_subset("Ch\\d+/\\d+[.]\\d+")
 
 new_names <- numbered_examples %>%
+  # "Ch02/2.3" => "Ch02/02.3" so file's chapter num matches folder's num
+  str_replace("(/Ch)(\\d+)(/)(\\d+)([.])", "\\1\\2\\3\\2\\5") %>%
   # "/16.6_" => "/16.06_" so filename-order matches presentation-order
   str_replace("(/\\d+[.])(\\d_)", "\\10\\2") %>%
   # "/16.06_" => "/16-06_" bc we don't want dots in filenames
   str_replace("(/\\d+)[.](\\d+_)", "\\1-\\2") %>%
   # Remove other symbols
-  str_replace_all(" [&] ", " and ") %>%
-  str_replace_all("\\^2", "-squared")
+  str_replace_all("[&]", "And") %>%
+  str_replace_all("\\^2", "Squared")
 
 file.rename(numbered_examples, to = new_names)
