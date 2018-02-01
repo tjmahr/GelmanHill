24b691d92c7bde100fbcaa4f92958f46b56d2919
Tristan Jay Mahr <Mahr@Wisc.Edu>
2018-02-01 15:45:08 -0600

    try out bayesian anova

--- examples/Ch22/22-01_ClassicalANOVA.R
+++ examples/Ch22/22-01_ClassicalANOVA.R
@@ -4,8 +4,8 @@ library(ggplot2)
 ## Read the pilots data & define variables
 # Data are at http://www.stat.columbia.edu/~gelman/arm/examples/pilots
 
-pilots <- read.table ("pilots.dat", header=TRUE)
-attach (pilots)
+pilots <- read.table("./examples/Ch22/pilots.dat", header = TRUE)
+attach(pilots)
 group.names <- as.vector(unique(group))
 scenario.names <- as.vector(unique(scenario))
 n.group <- length(group.names)
@@ -16,7 +16,7 @@ group.id <- NULL
 scenario.id <- NULL
 for (j in 1:n.group){
   for (k in 1:n.scenario){
-    ok <- group==group.names[j] & scenario==scenario.names[k]    
+    ok <- group==group.names[j] & scenario==scenario.names[k]
     successes <- c (successes, sum(recovered[ok]==1,na.rm=T))
     failures <- c (failures, sum(recovered[ok]==0,na.rm=T))
     group.id <- c (group.id, j)
@@ -41,4 +41,23 @@ treatment <- group.id
 airport <- scenario.id
 
 ## Classical anova of pilots data
-## summary (aov (y ~ factor (treatment) + factor(airport))) FIXME
+data <- data.frame(y, treatment = factor(treatment), airport = factor(airport))
+summary(aov(y ~ treatment + airport, data))
+plot(aov(y ~ treatment + airport, data))
+
+library(rstanarm)
+post1 <- stan_aov(
+  y ~ treatment + airport,
+  data,
+  prior = R2(location = 0.5))
+post1
+
+# Bayesian formulation
+post2 <- stan_glmer(
+  y ~ 1 + (1 | treatment) + (1 | airport),
+  family = gaussian,
+  data)
+post2
+
+# Compare to Figure 22.5
+bayesplot::mcmc_intervals(as.matrix(post2), regex_pars = "sigma|Sigma")
